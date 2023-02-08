58dad7563c4aaeb7377ceb98e1047ca8fcd8d35c
David Megginson <david.megginson@gmail.com>
2023-02-08 18:11:41 -0500

    REmove run-ufo script

--- run-ufo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-AIRPORT=$1
-if [ -z "$AIRPORT" ]; then
-    AIRPORT=CYRO
-fi
-
-fgfs --fg-scenery=/home/david/GIS/fgfs/fgfs-canada-us-scenery --enable-fullscreen --disable-sound --disable-real-weather-fetch --timeofday=noon --enable-hud --altitude=3000 --airport=$AIRPORT --aircraft=UFO "$@"
