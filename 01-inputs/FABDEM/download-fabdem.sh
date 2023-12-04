8e31445aa212cd0bbd0336cba57366de689c7519
David Megginson <david.megginson@gmail.com>
2023-12-04 12:40:02 -0500

    Fix hemispheres.

--- 01-inputs/FABDEM/download-fabdem.sh
+++ 01-inputs/FABDEM/download-fabdem.sh
@@ -16,18 +16,38 @@
 
 DIR=Downloads
 
-TEMPLATE='https://data.bris.ac.uk/datasets/s5hqmjcdj8yo2ibzi9b4ew3sn/N%02dW%03d-N%02dW%03d_FABDEM_V1-2.zip'
+TEMPLATE='https://data.bris.ac.uk/datasets/s5hqmjcdj8yo2ibzi9b4ew3sn/%s%02d%s%03d-%s%02d%s%03d_FABDEM_V1-2.zip'
 
-MIN_LAT=0
+MIN_LAT=-90
 MIN_LON=-180
 MAX_LAT=90
-MAX_LON=-0
+MAX_LON=0
+
+function dir_x () {
+    [ $1 -lt 0 ] && echo W || echo E
+}
+
+function dir_y () {
+    [ $1 -lt 0 ] && echo S || echo N
+}
+
+function abs () {
+    [ $1 -lt 0 ] && expr $1 '*' -1 || echo $1
+}
+
 
 lat=$MIN_LAT
 lon=$MIN_LON
 while [ $lat -lt $MAX_LAT ]; do
     while [ $lon -lt $MAX_LON ]; do
-        url=$(printf "$TEMPLATE" $lat `expr $lon '*' -1` `expr $lat + 10` `expr $lon '*' -1 - 10`)
+        max_lat=$(expr $lat + 10)
+        max_lon=$(expr $lon + 10)
+        url=$(printf "$TEMPLATE" \
+                     $(dir_y $lat) $(abs $lat) \
+                     $(dir_x $lon) $(abs $lon) \
+                     $(dir_y $max_lat) $(abs $max_lat) \
+                     $(dir_x $max_lon) $(abs $max_lon) )
+        #echo $lat $lon $max_lat $max_lon $url; exit
         file=$(echo "$url" | sed -e 's!^.*/!!')
         if [ ! -f $DIR/$file ]; then
             wget -c -P $DIR -v "$url" || ( rm -f "$DIR/$file" && exit 2)
