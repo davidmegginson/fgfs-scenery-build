5a7fddc50247c70c9222403871e7969e0fadce42
David Megginson <david.megginson@gmail.com>
2023-12-16 11:51:14 -0500

    Make dem filter more robust around poles and antimeridian

--- scripts/list-dem.py
+++ scripts/list-dem.py
@@ -1,15 +1,50 @@
+""" List DEM source files to use for a 10x10 bucket with gdalchop
+
+Include a 1 deg overlap on each side
+
+Usage:
+
+  python list-dem.py DEM_SOURCE_PATH BUCKET
+
+Where BUCKET is a 10x10 deg bucket name like w080n40.
+
+"""
+
 import pathlib, re, sys
 
+
 def parse_bucket (bucket):
-    """ Parse a bucket into its corners """
+    """ Parse a bucket into its bounds, with a 1 deg overlap in each direction"""
+
+    def norm_lon(lon):
+        while lon < -180:
+            lon += 180
+        while lon > 179:
+            lon -= 180
+        return lon
+
+    def norm_lat(lat):
+        if lat < -90:
+            lat = -90
+        elif lat > 90:
+            lat = 90
+        return lat
+    
     result = re.match(r'^([ew])(\d{3})([ns])(\d{2})$', bucket)
     if not result:
         raise Exception("Badly-formatted bucket " + bucket)
     min_lon = int(result.group(2)) * (1 if result.group(1) == 'e' else -1)
     min_lat = int(result.group(4)) * (1 if result.group(3) == 'n' else -1)
-    return (min_lon - 1, min_lat - 1, min_lon + 11, min_lat + 11,)
+    return (
+        norm_lon(min_lon - 1),
+        norm_lat(min_lat - 1),
+        norm_lon(min_lon + 11),
+        norm_lat(min_lat + 11),
+    )
+
 
 def get_bucket (lon, lat):
+    """ Get the 10x10 bucket that contains lon and lat (e.g. w080n40) """
     lon -= lon % 10
     lat -= lat % 10
     return "{}{:03d}{}{:02d}".format(
@@ -19,31 +54,67 @@ def get_bucket (lon, lat):
         abs(lat)
     )
 
-def check_dem (filename, bounds):
-    """ Get the bottom left corner of a dem """
+
+def check_in_bounds(lon, lat, bounds):
+    """ Check that a point appears in bounds 
+    Handles antimeridian (assumes smallest box)
+
+    """
+    (min_lon, min_lat, max_lon, max_lat,) = bounds
+
+    # latitude is easy
+    if lat < min_lat or lat > max_lat:
+        return False
+
+    # longitude is trickier because of the antimeridian
+    if abs(max_lon - min_lon) >= 180:
+        # bounds cross antimeridian
+        return (lon > max_lon or lon < min_lon)
+    else:
+        # bounds do not cross antimeridian
+        return not (lon > max_lon or lon < min_lon)
+
+
+def dem_file_in_bounds_p (filename, bounds):
+    """ Check a DEM filename against the bounds
+    The filename is in the format N00W000 (etc) with a .tif or .hgt extension
+    
+    """
     result = re.match(r'([NS])(\d{2})([EW])(\d{3}).*\.(tif|hgt)$', filename)
     if not result:
         raise Exception("Badly-formatted DEM name " + filename)
     lat = int(result.group(2)) * (1 if result.group(1) == 'N' else -1)
     lon = int(result.group(4)) * (1 if result.group(3) == 'E' else -1)
-    return (lat >= bounds[1] and lat < bounds[3] and lon >= bounds[0] and lon < bounds[2])
+    return check_in_bounds(lon, lat, bounds)
 
-def find_files (input_dir, bounds):
+
+def find_matching_dem_files (input_dir, bounds):
+    """ Return a sorted list of DEM files for the 10x10 bucket 
+
+    File names will be absolute.
+    """
     files = []
     path = pathlib.Path(input_dir)
     for glob in ('*.tif', '*.hgt',):
         for entry in path.rglob(glob):
-            if entry.is_file() and (entry.name.endswith('.hgt') or entry.name.endswith('.tif')) and check_dem(entry.name, bounds):
+            if entry.is_file() and dem_file_in_bounds_p(entry.name, bounds):
                 files.append(entry.absolute())
     return sorted(files)
 
-if len(sys.argv) != 3:
-   print("Usage: {} INPUT_DIR BUCKET".format(sys.argv[0]), file=sys.stderr)
-   sys.exit(2)
 
-INPUT_DIR=sys.argv[1]
-BUCKET=sys.argv[2]
+#
+# Run the script from the command line
+#
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+       print("Usage: {} INPUT_DIR BUCKET".format(sys.argv[0]), file=sys.stderr)
+       sys.exit(2)
 
-files = find_files(INPUT_DIR, parse_bucket(BUCKET))
-for file in files:
-    print(file)
+    INPUT_DIR=sys.argv[1]
+    BUCKET=sys.argv[2]
+
+    files = find_matching_dem_files(INPUT_DIR, parse_bucket(BUCKET))
+    for file in files:
+        print(file)
+
+    exit(0)
