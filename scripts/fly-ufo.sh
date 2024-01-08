84001363961a4fda92d48c8c67b4a30ab6c6a022
David Megginson <david.megginson@gmail.com>
2024-01-08 10:49:53 -0500

    Update for scenery-folder renaming.

--- scripts/fly-ufo.sh
+++ scripts/fly-ufo.sh
@@ -9,5 +9,5 @@ BASEDIR=$(cd $(dirname $0)/.. && pwd)
 fgfs --no-default-config --disable-sound --disable-ai-traffic \
      --disable-clouds --disable-clouds3d --disable-real-weather-fetch \
      --timeofday=morning --read-only=1 \
-     --enable-hud --fg-scenery=$BASEDIR/04-output/fgfs-canada-us-scenery/ \
+     --enable-hud --fg-scenery=$BASEDIR/04-output/fgfs-americas-scenery/ \
      --aircraft=ufo "$@" --heading=0 --altitude=3000
