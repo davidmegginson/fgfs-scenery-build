ff68283d96a8072eb62cdcfa14e1ab0131252671
David Megginson <david.megginson@gmail.com>
2024-09-11 18:20:53 -0400

    Added local time start to ufo script

--- scripts/fly-ufo.sh
+++ scripts/fly-ufo.sh
@@ -21,7 +21,7 @@ fgfs --no-default-config \
      --prop:string:/sim/thread-cpu-affinity=osg \
      --enable-hud --save-on-exit=1 \
      --fullscreen=1 --texture-filtering=16 --texture-cache=1 \
-     --start-date-gmt=2023:06:30:16:00:00 --terrasync=0 \
+     --start-date-lat=2023:06:30:12:00:00 \
      --generic=broadcast,out,10,255.255.255.255,49002,foreflight-xatt \
      --generic=broadcast,out,1,255.255.255.255,49002,foreflight-xgps \
      --aircraft=ufo --heading=0 --altitude=3000 "$@"
